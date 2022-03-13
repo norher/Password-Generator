410336d88c1add6f1b4380d47ca11cf961215820
Norman Herrera <norman.herrera@ymail.com>
2022-03-12 19:58:38 -0800

    added concat functions to confirms for characters

--- Assets/script.js
+++ Assets/script.js
@@ -1,41 +1,74 @@
-//Made arrays of all the character required for the password
+//Made functions for each special character
+// Maybe don't need this at all
+// function getRandomLower() {
+//   return lowerAlphabet[Math.floor(Math.random(lowerAlphabet) * 26)];
+// }
 
-const lowerAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+// function generatePassword() {
+  //   var possiblePassword = [];
+  
+  //   length();
+  //   hasNumbers();
+  //   if (hasNumbers) {
+  //     possibleCharacters = [...possibleCharacters, ...numberChoices];
 
-const upperAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+  //   if (possibleCharacters.length < 8) return "Please choose a number between 8 and 128";
+  //   if (possibleCharacters.length > 128) return "Please choose a number between 8 and 128";
+  
+  //   for (let i = 0; i < length; i++){
+  //     Math.floor[math.random(possibleCharacters) * possibleCharacters.length];
+  //   }
+  //   return "Yay";
+  // }
 
-const numberChoices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-
-const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "_"]
-
-function addLower() {
-  confirm("Let's get your password started\nWould you like lowercase letters?\nClick OK for yes, Cancel for no.");
-};
-
-function addUpper() {
-  confirm("Okay, now, would you like Uppercase letters?");
-};
-
-function addNumbers() {
-  confirm("Okay, now, would you like to add numbers?");
-};
-
-function addSpecial() {
-  confirm("Okay, now, would you like special characters?");
-};
-
-function howLong() {
-  prompt("Okay, finally, How long would you like your password to be? Pick any length, between 8 and 128");
-};
-
-function startPassword() {
-  addLower();
-  addUpper();
-  addNumbers();
-  addSpecial();
-  howLong();
-  return;
+function length() {
+  prompt("Let's get your password started\nHow long would you like your password.");
+  // if (length < 8) {
+  //   alert("Please select a number between 8 and 128");
+  // }
+  // if (length > 128) {
+  //   alert("Please select a number between 8 and 128");
+  // }
+  // if (length !== Number) {
+  //   alert("Please select a number between 8 and 128");
+  // }
+  hasLower();
 }
+
+const lowerAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+const upperAlphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+const numberChoices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+
+const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "_"];
+
+// const pwLength = howLong()
+
+var yourPassword = "";
+
+function generatePassword() {
+  let possiblePassword = [];
+  console.log(possiblePassword);
+  if (confirm('would you like uppercase?\nClick Ok for yes, or Cancel for No') == true) {
+    possiblePassword = possiblePassword.concat(upperAlphabet);
+  };
+  console.log(possiblePassword);
+  if (confirm('would you like lowercase?') == true) {
+    possiblePassword = possiblePassword.concat(lowerAlphabet);
+  };
+   console.log(possiblePassword);
+  if (confirm('would you like specialCharacter?') == true) {
+    possiblePassword = possiblePassword.concat(specialCharacters);
+  };
+    console.log(possiblePassword);
+  if (confirm('would you like numbers?') == true) {
+    possiblePassword = possiblePassword.concat(numberChoices);
+  };
+  console.log(possiblePassword);
+  let pwLength = prompt('How long would you like your password to be?\n Please choose a number between 8 and 128 ');
+  console.log(pwLength.length);
+  };
 
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
@@ -49,60 +82,5 @@ function writePassword() {
 
 }
 
-  //     const length = +howLong.value;
-//     const hasLower = addLower;
-//     const hasUpper = addUpper;
-//     const hasNumber = addNumbers;
-//     const hasSpecial = addSpecial;
-// }
-
 // Add event listener to generate button
-generateBtn.addEventListener("click", startPassword)
-
-
-//Made functions for each special character
-
-function getRandomLower() {
-  return lowerAlphabet[Math.floor(Math.random(lowerAlphabet) * 26)];
-}
-function getRandomUpper() {
-  return upperAlphabet[Math.floor(Math.random(upperAlphabet) * 26)];
-}
-function getRandomCharacter() {
-  return specialCharacters[Math.floor(Math.random(specialCharacters) * 20)];
-}
-function getRandomNumber() {
-  return numberChoices[Math.floor(Math.random(numberChoices) * 10)];
-}
-
-//Store functions within an object
-
-const randomFunction = {
-  lower: getRandomLower,
-  upper: getRandomUpper,
-  number: getRandomNumber,
-  special: getRandomCharacter,
-}
-
-// console.log(getRandomCharacter());
-
-//Dom Elements
-
-const resultEl = document.getElementById('result');
-const lengthEl = document.getElementById('length');
-const uppercaseEl = document.getElementById('uppercase');
-const lowercaseEl = document.getElementById('lowercase');
-const numbersEl = document.getElementById('numbers');
-const symbolsEl = document.getElementById('symbols');
-const generateEl = document.getElementById('generate');
-
-
-// if (startPassword()==true) {
-//   yourPassword.push(lowerAlphabet)
-// }
-
-//   const length = +howLong.value;
-//   const hasLower = addLower;
-//   const hasUpper = addUpper;
-//   const hasNumber = addNumbers;
-//   const hasSpecial = addSpecial;
+generateBtn.addEventListener("click", generatePassword)
