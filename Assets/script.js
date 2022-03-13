0201f423ffd51b4e43e5abfd0c392f603d6fef2a
Norman Herrera <norman.herrera@ymail.com>
2022-03-12 23:44:11 -0800

    added for loop for password

--- Assets/script.js
+++ Assets/script.js
@@ -1,39 +1,14 @@
-//Made functions for each special character
-// Maybe don't need this at all
-// function getRandomLower() {
-//   return lowerAlphabet[Math.floor(Math.random(lowerAlphabet) * 26)];
+//   if (howLong < 8 || howLong > 128) {
+//     alert("Please select a number between 8 and 128");
+//     howLong();
+//   };
+//   if (howLong !== Number) {
+//     alert("Please select a number between 8 and 128");
+//     howLong();
+//   } else {
+//     alert('Okay, your password will be that long');
+//   };
 // }
-
-// function generatePassword() {
-  //   var possiblePassword = [];
-  
-  //   length();
-  //   hasNumbers();
-  //   if (hasNumbers) {
-  //     possibleCharacters = [...possibleCharacters, ...numberChoices];
-
-  //   if (possibleCharacters.length < 8) return "Please choose a number between 8 and 128";
-  //   if (possibleCharacters.length > 128) return "Please choose a number between 8 and 128";
-  
-  //   for (let i = 0; i < length; i++){
-  //     Math.floor[math.random(possibleCharacters) * possibleCharacters.length];
-  //   }
-  //   return "Yay";
-  // }
-
-function length() {
-  prompt("Let's get your password started\nHow long would you like your password.");
-  // if (length < 8) {
-  //   alert("Please select a number between 8 and 128");
-  // }
-  // if (length > 128) {
-  //   alert("Please select a number between 8 and 128");
-  // }
-  // if (length !== Number) {
-  //   alert("Please select a number between 8 and 128");
-  // }
-  hasLower();
-}
 
 const lowerAlphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
@@ -43,12 +18,15 @@ const numberChoices = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 
 const specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "+", "-", ".", "`", "~", "|", "<", ">", "=", "_"];
 
-// const pwLength = howLong()
+var generatedPassword = '';
 
-var yourPassword = "";
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
+// generate password function
 function generatePassword() {
-  let possiblePassword = [];
+  var possiblePassword = '';
+
   console.log(possiblePassword);
   if (confirm('would you like uppercase?\nClick Ok for yes, or Cancel for No') == true) {
     possiblePassword = possiblePassword.concat(upperAlphabet);
@@ -66,20 +44,24 @@ function generatePassword() {
     possiblePassword = possiblePassword.concat(numberChoices);
   };
   console.log(possiblePassword);
-  let pwLength = prompt('How long would you like your password to be?\n Please choose a number between 8 and 128 ');
-  console.log(pwLength.length);
-  };
+  
+  var pwLength = +prompt('How long would you like your password to be?\n Please choose a number between 8 and 128')
+  console.log(pwLength, typeof possiblePassword);
 
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
+  let yourPassword = [];
+  for (var i = 0; i < pwLength; i++) {
+     yourPassword = yourPassword.concat(possiblePassword[Math.floor(Math.random() * possiblePassword.length)]);
+  }
+  console.log(typeof yourPassword);
+  return yourPassword;
+};
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var yourPassword = generatePassword();
   var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+  passwordText.value = yourPassword;
+  console.log(yourPassword);
 }
 
 // Add event listener to generate button
